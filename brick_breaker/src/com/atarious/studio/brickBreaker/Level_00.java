4016c9790a8c619154403e39559f47c61ffb1cd9
Abdullah Ramees <73293868+AbdullahRamees@users.noreply.github.com>
2021-11-11 19:48:23 +0530

    Brick breaker (#2)
    * main class
    
    * painting probs

---
+++ brick_breaker/src/com/atarious/studio/brickBreaker/Level_00.java
@@ -0,0 +1,4 @@
+package com.atarious.studio.brickBreaker;
+
+public class Level_00 {
+}
---
+++ brick_breaker/src/com/atarious/studio/brickBreaker/Main.java
@@ -0,0 +1,19 @@
+package com.atarious.studio.brickBreaker;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] arg){
+        JFrame frame = new JFrame();                                //creating an object of JFrame class
+
+        Game game = new Game();                                     //creating an object of game class
+
+        //frame properties
+        frame.setBounds(10,10,700,600);           //boundaries of frame
+        frame.setTitle("Atarious Studio's Brick Breaker");          //title of frame
+        frame.setResizable(false);                                  //resizing the frame is false
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       //exit when close button pressed
+        frame.add(game);                                            //adding the penal
+    }
+}
