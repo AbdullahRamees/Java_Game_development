44fccb7e301b1f9ffdb3bd741e2d273aedea9b3e
Abdullah Ramees <73293868+AbdullahRamees@users.noreply.github.com>
2021-11-13 20:57:20 +0530

    Brick breaker (#3)
    * main class
    
    * painting probs
    
    * level craetion
    
    * brick function finished

--- brick_breaker/src/com/atarious/studio/brickBreaker/Game.java
+++ brick_breaker/src/com/atarious/studio/brickBreaker/Game.java
@@ -21,13 +21,18 @@ public class Game extends JPanel implements KeyListener,ActionListener {
         private int playerMove = 20;                         //action position when user hit key
 
         //playable ball prob properties
-        private int BallPosX = 120;                          //starting position of ball X axis
-        private int BallPosY = 350;                          //starting position of Ball Y axis
-        private int BallX = -1;                              //ball traverse distance along X axis
-        private int BallY = -2;                              //Ball traverse distance along Y axis
+
+        private int BallPosX = 120;                              //starting position of ball X axis
+        private int BallPosY = 350;                              //starting position of Ball Y axis
+        private int BallMoveX = -1;                              //ball traverse distance along X axis
+        private int BallMoveY = -2;                              //Ball traverse distance along Y axis
+
+        private Level_00 map;
 
         //constructor of Game class
         public Game (){
+            map=new Level_00(3,7);
+
             addKeyListener(this);
             setFocusable(true);
             setFocusTraversalKeysEnabled(false);
@@ -40,6 +45,8 @@ public class Game extends JPanel implements KeyListener,ActionListener {
             graphics.setColor(Color.gray);
             graphics.fillRect(1,1,692,592);
 
+            map.draw((Graphics2D) graphics);
+          
             //paint boarder of frame
             graphics.setColor(Color.red);
             graphics.fillRect(0,0,3,592);   //left boarder
@@ -57,16 +64,6 @@ public class Game extends JPanel implements KeyListener,ActionListener {
 
         }
 
-        //peddle  movement  methods
-        public void moveleft (){
-            Play=true;
-            PlayerX -= playerMove;
-        }
-        public void moveRight (){
-            Play=true;
-            PlayerX += playerMove;
-        }
-
 
 
 
@@ -75,6 +72,52 @@ public class Game extends JPanel implements KeyListener,ActionListener {
         public void actionPerformed(ActionEvent e) {
             //repainting the event action
             timer.start();
+
+            //ball movement
+                if(Play){
+                    if(new Rectangle(BallPosX,BallPosY,20,20).intersects
+                            (new Rectangle(PlayerX,550,100,8))){
+                        BallMoveY = -BallMoveY;
+                    }
+                    for (int i =0;i<map.Map.length;i++){
+                        for(int j=0;j<map.Map[0].length;i++){
+                           int brickX=map.MapWidth+80;
+                           int brickY=map.mapheight+50;
+                           int brickWidth=map.MapWidth;
+                           int brickHeight=map.mapheight;
+
+                           Rectangle rect=new Rectangle(brickX,brickY,brickWidth,brickHeight);
+                           Rectangle ball=new Rectangle(BallPosX,BallPosY,20,20);
+                           Rectangle brick=rect;
+
+                           if(ball.intersects(brick)){
+                               map.setbrickvalue(0,i,j);
+                               TotalBrick--;
+                               scores=+5;
+                               if(BallPosX+19<=brick.x||BallPosX+1>= brick.x+brick.width){
+                                   BallMoveX= -BallMoveX;
+                               }else{
+                                   BallMoveY= -BallMoveY;
+                               }
+                           }
+
+                        }
+                    }
+
+
+                    BallPosX += BallMoveX;
+                    BallPosY += BallMoveY;
+                    if(BallPosX==10){
+                        BallMoveX = -BallMoveX;
+                    }
+                    if(BallPosY==10){
+                        BallMoveY = -BallMoveY;
+                    }
+                    if(BallPosX>670){
+                        BallMoveX = -BallMoveX;
+                    }
+                }
+
             repaint();
 
         }
@@ -106,7 +149,18 @@ public class Game extends JPanel implements KeyListener,ActionListener {
 
         }
 
-        @Override
+        //peddle  movement  methods
+        public void moveleft (){
+            Play=true;
+            PlayerX =PlayerX - playerMove;
+        }
+        public void moveRight (){
+            Play=true;
+            PlayerX =PlayerX + playerMove;
+        }
+
+
+    @Override
         public void keyReleased(KeyEvent e) {
 
         }
